b537873113ab4eaffc2aaba1ad9a99e1e2f4c906
drmth <79118141+drmth@users.noreply.github.com>
2025-06-19 14:32:07 +0200

    feat: dashboard. Adapt route to front request

--- backend/src/controllers/dashboard.ts
+++ backend/src/controllers/dashboard.ts
@@ -2,153 +2,206 @@ import { Hono } from "hono";
 import prisma from "../db/client";
 import { describeRoute } from "hono-openapi";
 import { response200 } from "../utils/openapi";
+import { dashboardSchema, Expense } from "../validators/dashboard";
 
 const dashboardRouter  = new Hono();
 
 dashboardRouter
   .basePath('/dashboard')
   .get(
-    '/budget',
+    '/',
+    describeRoute({
+      tags: ['dashboard'],
+      summary: 'Get dashboard data',
+      description: 'Retrieve dashboard data.',
+      responses: {
+        200: response200(dashboardSchema),
+      },
+    }),
     async (c) => {
     const userId = c.get('jwtPayload').userId;
     const now = new Date();
-    const year = c.req.query('year') ? parseInt(c.req.query('year')!, 10) : now.getFullYear();
-    const month = c.req.query('month') ? parseInt(c.req.query('month')!, 10) : now.getMonth() + 1;
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
 
-    const [budgets, total] = await Promise.all([
-      prisma.budget.findMany({
-      where: {
-        userId: userId,
-        year: year,
-        month: month,
-      },
-      omit:{
-        id: true,
-        categoryId: true,
-        createdAt: true,
-        updatedAt: true,
-        userId: true,
-      },
-      include: {
-        expenses: {
-          omit: {
-            id: true,
-            createdAt: true,
-            updatedAt: true,
-            userId: true,
-            budgetId: true,
-          }
-        },
-        category: {
-          omit: {
-            id: true,
-            createdAt: true,
-            updatedAt: true,
-            userId: true,
+    const [
+      currentMonthExpenses, 
+      last6MonthsExpenses, 
+      todayExpenses, 
+      currentWeekExpenses, 
+      previousMonthExpenses, 
+      averageMonthlyExpenses, 
+      currentMonthIncome, 
+      currentMonthBudget, 
+      last10Expenses
+    ] = await Promise.all([
+      prisma.expense.aggregate({
+        _sum: { amount: true },
+        where: {
+          userId,
+          date: {
+            gte: new Date(year, month - 1, 1),
+            lt: new Date(year, month, 1),
           },
-          include: {
-            color: {
-              omit: {
-                id: true,
-                createdAt: true,
-                updatedAt: true,
+        },
+      }),
+      prisma.expense.findMany({
+        where: {
+          userId,
+          date: {
+            gte: new Date(year, month - 6, 1),
+            lt: new Date(year,  month, 0),
+          },
+        },
+        omit: {
+          id: true,
+          createdAt: true,
+          updatedAt: true,
+          userId: true,
+          budgetId: true,
+        },
+      }),
+      prisma.expense.aggregate({
+        _sum: { amount: true },
+        where: {
+          userId,
+          date: {
+            gte: new Date(year, month - 1, now.getDate()),
+            lt: new Date(year, month - 1, now.getDate() + 1),
+          },
+        },
+      }),
+      prisma.expense.aggregate({
+        _sum: { amount: true },
+        where: {
+          userId,
+          date: {
+            gte: new Date(year, month - 1, now.getDate() - 6),
+            lt: new Date(),
+          },
+        },
+      }),
+      prisma.expense.aggregate({
+        _sum: { amount: true },
+        where: {
+          userId,
+          date: {
+            gte: new Date(year, month - 2, 1),
+            lt: new Date(year, month - 1, 1),
+          },
+        },
+      }),
+      prisma.expense.aggregate({
+        _avg: { amount: true },
+        where: {
+          userId,
+        },
+      }),
+      prisma.income.aggregate({
+        _sum: { value: true },
+        where: {
+          userId,
+          year,
+          month,
+        },
+      }),
+      prisma.budget.aggregate({
+        _sum: { amount: true },
+        where:{
+          userId,
+          year,
+          month
+        }
+      }),
+      prisma.expense.findMany({
+        where: {
+          userId,
+        },
+        orderBy: {
+          date: 'desc',
+        },
+        omit: {
+          id: true,
+          createdAt: true,
+          updatedAt: true,
+          userId: true,
+          budgetId: true,
+        },
+        include: {
+          budget: {
+            omit: {
+              id: true,
+              userId: true,
+              categoryId: true,
+              createdAt: true,
+              updatedAt: true,
+            },
+            include: {
+              category: {
+                omit: {
+                  userId: true,
+                  id: true,
+                  colorId: true,
+                  createdAt: true,
+                  updatedAt: true,
+                },
+                include: {
+                  color: {
+                    omit: {
+                      id: true,
+                      createdAt: true,
+                      updatedAt: true,
+                    }
+                  }
+                }
               }
             }
-          },
+          }
         },
-      },
-    }),
-    prisma.budget.aggregate({
-      _sum: {
-        amount: true,
-      },
-      where: {
-        userId: userId,
-        year: year,
-        month: month,
-      },
-    })
+        take: 10,
+      }),
     ]);
 
-    return c.json({budgets, total}, 200);
-  })
-  .get(
-    '/income',
-    async (c) => {
-    const userId = c.get('jwtPayload').userId;
-    const now = new Date();
-    const year = c.req.query('year') ? parseInt(c.req.query('year')!, 10) : now.getFullYear();
-    const month = c.req.query('month') ? parseInt(c.req.query('month')!, 10) : now.getMonth() + 1;
+    const last6MonthsExpensesByMonth = formatLast6MonthsExpensesByMonth(last6MonthsExpenses);
 
-    const [incomes, total] = await Promise.all([
-      prisma.income.findMany({
-      where: {
-        userId: userId,
-        year: year,
-        month: month,
-      },
-      omit:{
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-        userId: true,
-      },
-    }),
-    prisma.income.aggregate({
-      _sum: {
-        value: true,
-      },
-      where: {
-        userId: userId,
-        year: year,
-        month: month,
-      },
-    })
-    ]);
-
-    return c.json({incomes, total}, 200);
-  })
-  .get(
-    '/expense',
-    async (c) => {
-    const userId = c.get('jwtPayload').userId;
-    const year = c.req.query('year') ? parseInt(c.req.query('year')!, 10) : new Date().getFullYear();
-    const month = c.req.query('month') ? parseInt(c.req.query('month')!, 10) : new Date().getMonth() + 1;
-    const startDate = new Date(year, month - 1, 1);
-    const endDate = new Date(year, month, 0);
-
-    const [expenses, total] = await Promise.all([
-      prisma.expense.findMany({
-      where: {
-        userId: userId,
-        date: {
-          gte: startDate,
-          lt: endDate,
-        },
-      },
-      omit:{
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-        budgetId: true,
-        userId: true,
-      },
-    }),
-    prisma.expense.aggregate({
-      _sum: {
-        amount: true,
-      },
-      where: {
-        userId: userId,
-        date: {
-          gte: startDate,
-          lt: endDate,
-        },
-      },
-    })
-    ]);
-
-    return c.json({expenses, total}, 200);
+    return c.json({
+      currentMonthExpenses: currentMonthExpenses._sum.amount || 0,
+      last6MonthsExpensesByMonth: last6MonthsExpensesByMonth,
+      todayExpenses: todayExpenses._sum.amount || 0,
+      currentWeekExpenses: currentWeekExpenses._sum.amount || 0,
+      previousMonthExpenses: previousMonthExpenses._sum.amount || 0,
+      averageMonthlyExpenses: averageMonthlyExpenses?._avg?.amount || 0,
+      currentMonthRevenue: currentMonthIncome._sum.value || 0,
+      currentMonthBudget:  currentMonthBudget?._sum?.amount || 0,
+      last10Expenses: last10Expenses}, 200);
   });
+
+function formatLast6MonthsExpensesByMonth(
+  last6MonthsExpenses: Expense[]
+): Record<string, number> {
+  const result: Record<string, number> = {};
+  const now = new Date();
+
+    for (let i = 5; i >= 0; i--) {
+      const date = new Date(now.getFullYear(), now.getMonth() - i, 1);
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      const key = `${month}/${year}`;
+      result[key] = 0;
+    }
+
+    for (const expense of last6MonthsExpenses) {
+      const date = new Date(expense.date);
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      const key = `${month}/${year}`;
+
+      if (key in result) {
+        result[key] += expense.amount;
+      }
+    }
+
+    return result;
+}
 
 export default dashboardRouter;
