e329ddeb3f48b3a5601603a1af6c3ce95ec104da
Eric31300 <eric.ponchant@wanadoo.fr>
2025-07-18 13:49:17 +0200

    feat(meta): dynamic meta for Hompage and Terms of Service

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,19 +3,26 @@ import Footer from '@/components/homepage/Footer';
 import Header from '@/components/homepage/Header';
 import HeroSection from '@/components/homepage/HeroSection';
 import ProblemSection from '@/components/homepage/ProblemSection';
+import { PageMeta } from '@/components/PageMeta';
 import { DefaultWrapper } from '@/layouts/DefaultWrapper';
 
 const Home = () => {
   return (
-    <DefaultWrapper>
-      <div className="w-full place-items-center ">
-        <Header />
-        <HeroSection />
-        <ProblemSection />
-        <Contact />
-        <Footer />
-      </div>
-    </DefaultWrapper>
+    <>
+      <PageMeta
+        titleKey="home.meta.title"
+        descriptionKey="home.meta.description"
+      />
+      <DefaultWrapper>
+        <div className="w-full place-items-center ">
+          <Header />
+          <HeroSection />
+          <ProblemSection />
+          <Contact />
+          <Footer />
+        </div>
+      </DefaultWrapper>
+    </>
   );
 };
 
