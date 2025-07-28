8bfda43833914b928665958b74dfcd4c32bc55e1
Dxqeee <thibault62118@gmail.com>
2025-07-28 04:58:07 +0200

    fix: logout cookies deletion in production

--- backend/src/lib/tokens.ts
+++ backend/src/lib/tokens.ts
@@ -1,22 +1,22 @@
-import crypto from 'node:crypto';
-import type { Context } from 'hono';
-import { deleteCookie, setCookie, setSignedCookie } from 'hono/cookie';
-import { HTTPException } from 'hono/http-exception';
-import { sign } from 'hono/jwt';
-import { getEnv } from '../utils/env';
+import crypto from "node:crypto";
+import type { Context } from "hono";
+import { deleteCookie, setCookie, setSignedCookie } from "hono/cookie";
+import { HTTPException } from "hono/http-exception";
+import { sign } from "hono/jwt";
+import { getEnv } from "../utils/env";
 
 export async function generateTokenJWT(
 	id: string,
 	isAlertActive: boolean,
 	c: Context,
 ): Promise<string> {
-	const { SECRET_JWT, TOKEN_JWT_NAME, NODE_ENV } = getEnv();
+	const { SECRET_JWT, TOKEN_JWT_NAME, NODE_ENV, DOMAIN_NAME } = getEnv();
 
 	// 7 days expiration for JWT
 	const AUTH_EXPIRATION_DAYS = 7;
 	const AUTH_EXPIRATION_SECONDS = 60 * 60 * 24 * AUTH_EXPIRATION_DAYS;
 	const AUTH_EXPIRATION_MS = AUTH_EXPIRATION_SECONDS * 1000;
-	const isProduction = NODE_ENV === 'production';
+	const isProduction = NODE_ENV === "production";
 
 	const payload = {
 		userId: id,
@@ -26,7 +26,7 @@ export async function generateTokenJWT(
 
 	if (!SECRET_JWT) {
 		throw new HTTPException(500, {
-			message: 'JWT secret is not set.',
+			message: "JWT secret is not set.",
 		});
 	}
 
@@ -35,30 +35,32 @@ export async function generateTokenJWT(
 	await setSignedCookie(c, TOKEN_JWT_NAME, token, SECRET_JWT, {
 		httpOnly: true,
 		secure: isProduction,
-		sameSite: isProduction ? 'none' : 'lax',
+		sameSite: isProduction ? "none" : "lax",
 		expires: new Date(Date.now() + AUTH_EXPIRATION_MS),
-		path: '/',
+		path: "/",
+		domain: DOMAIN_NAME,
 	});
 
 	return token;
 }
 
 export async function generateTokenCSRF(c: Context): Promise<string> {
-	const { TOKEN_CSRF_NAME, NODE_ENV } = getEnv();
+	const { TOKEN_CSRF_NAME, NODE_ENV, DOMAIN_NAME } = getEnv();
 	const csrfToken = generateRandomString();
 
 	//1 hour expiration for CSRF token
 	const AUTH_EXPIRATION_MINUTES = 60;
 	const AUTH_EXPIRATION_SECONDS = 60 * AUTH_EXPIRATION_MINUTES;
 	const AUTH_EXPIRATION_MS = AUTH_EXPIRATION_SECONDS * 1000;
-	const isProduction = NODE_ENV === 'production';
+	const isProduction = NODE_ENV === "production";
 
 	setCookie(c, TOKEN_CSRF_NAME, csrfToken, {
 		httpOnly: true,
 		secure: isProduction,
-		sameSite: isProduction ? 'none' : 'lax',
+		sameSite: isProduction ? "none" : "lax",
 		expires: new Date(Date.now() + AUTH_EXPIRATION_MS),
-		path: '/',
+		path: "/",
+		domain: DOMAIN_NAME,
 	});
 
 	return csrfToken;
@@ -68,18 +70,18 @@ export function deleteUserCookie(c: Context) {
 	const { COOKIE_SECURE, DOMAIN_NAME } = getEnv();
 
 	deleteCookie(c, getEnv().TOKEN_JWT_NAME, {
-		path: '/',
+		path: "/",
 		secure: COOKIE_SECURE,
 		domain: DOMAIN_NAME,
 	});
 
 	deleteCookie(c, getEnv().TOKEN_CSRF_NAME, {
-		path: '/',
+		path: "/",
 		secure: COOKIE_SECURE,
 		domain: DOMAIN_NAME,
 	});
 }
 
 export function generateRandomString() {
-	return crypto.randomBytes(128).toString('base64url');
+	return crypto.randomBytes(128).toString("base64url");
 }
--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,69 +1,69 @@
-import { createMiddleware } from 'hono/factory';
-import { verify } from 'hono/jwt';
-import { getEnv } from '../utils/env';
-import { HTTPException } from 'hono/http-exception';
-import { getCookie, getSignedCookie } from 'hono/cookie';
-import { Context } from 'hono';
+import type { Context } from "hono";
+import { getCookie, getSignedCookie } from "hono/cookie";
+import { createMiddleware } from "hono/factory";
+import { HTTPException } from "hono/http-exception";
+import { verify } from "hono/jwt";
+import { getEnv } from "../utils/env";
 
 const { SECRET_JWT, TOKEN_CSRF_NAME, TOKEN_JWT_NAME } = getEnv();
 
 export const updateJWTPayload = async (c: Context) => {
-  const token = await getSignedCookie(c, SECRET_JWT, TOKEN_JWT_NAME);
+	const token = await getSignedCookie(c, SECRET_JWT, TOKEN_JWT_NAME);
 
-  if (!token) {
-    throw new HTTPException(401, {
-      message: 'You are not logged in.',
-    });
-  }
-  const payload = await authentify(token, SECRET_JWT, c);
-  c.set('jwtPayload', payload);
+	if (!token) {
+		throw new HTTPException(401, {
+			message: "You are not logged in.",
+		});
+	}
+	const payload = await authentify(token, SECRET_JWT, c);
+	c.set("jwtPayload", payload);
 };
 
 export const isAuthenticated = createMiddleware(async (c, next) => {
-  const token = await getSignedCookie(c, SECRET_JWT, TOKEN_JWT_NAME);
+	const token = await getSignedCookie(c, SECRET_JWT, TOKEN_JWT_NAME);
 
-  if (!token) {
-    throw new HTTPException(401, {
-      message: 'You are not logged in.',
-    });
-  }
-  const payload = await authentify(token, SECRET_JWT, c);
+	if (!token) {
+		throw new HTTPException(401, {
+			message: "You are not logged in.",
+		});
+	}
+	const payload = await authentify(token, SECRET_JWT, c);
 
-  c.set('jwtPayload', payload);
+	c.set("jwtPayload", payload);
 
-  await next();
+	await next();
 });
 
 export async function authentify(
-  token: string,
-  SECRET_JWT: string,
-  c: Context,
+	token: string,
+	SECRET_JWT: string,
+	c: Context,
 ) {
-  const decodedJWT = await verify(token, SECRET_JWT);
-  if (!decodedJWT) {
-    throw new HTTPException(401, {
-      message: 'Invalid JWT token.',
-    });
-  }
+	const decodedJWT = await verify(token, SECRET_JWT);
+	if (!decodedJWT) {
+		throw new HTTPException(401, {
+			message: "Invalid JWT token.",
+		});
+	}
 
-  if (['POST', 'DELETE', 'PATCH', 'PUT'].includes(c.req.method)) {
-    const csrfTokeFromHeader = c.req.header('csrf_token');
+	if (["POST", "DELETE", "PATCH", "PUT"].includes(c.req.method)) {
+		const csrfTokeFromHeader = c.req.header("csrf_token");
 
-    const csrfTokenFromCookie = getCookie(c, TOKEN_CSRF_NAME);
+		const csrfTokenFromCookie = getCookie(c, TOKEN_CSRF_NAME);
 
-    if (!csrfTokeFromHeader) {
-      throw new HTTPException(401, {
-        message: 'Missing CSRF token.',
-      });
-    }
+		if (!csrfTokeFromHeader) {
+			throw new HTTPException(401, {
+				message: "Missing CSRF token.",
+			});
+		}
 
-    if (csrfTokeFromHeader !== csrfTokenFromCookie) {
-      // If the CSRF token is not present or does not match, throw an error
-      throw new HTTPException(401, {
-        message: 'Invalid CSRF token.',
-      });
-    }
-  }
+		if (csrfTokeFromHeader !== csrfTokenFromCookie) {
+			// If the CSRF token is not present or does not match, throw an error
+			throw new HTTPException(401, {
+				message: "Invalid CSRF token.",
+			});
+		}
+	}
 
-  return decodedJWT;
+	return decodedJWT;
 }
