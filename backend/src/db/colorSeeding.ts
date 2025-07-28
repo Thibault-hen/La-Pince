a8134fedfb2ddab92b75510629d22941163a17de
Dxqeee <thibault62118@gmail.com>
2025-07-28 03:35:38 +0200

    fix: await color seeding

--- backend/src/db/colorSeeding.ts
+++ backend/src/db/colorSeeding.ts
@@ -1,25 +1,25 @@
-import prisma from './client';
+import prisma from "./client";
 
 const colorSeeding = async (): Promise<void> => {
-  await prisma.color.createMany({
-    data: [
-      { name: 'color.red', value: '#ef4444' },
-      { name: 'color.green', value: '#22c55e' },
-      { name: 'color.blue', value: '#3b82f6' },
-      { name: 'color.yellow', value: '#eab308' },
-      { name: 'color.violet', value: '#8b5cf6' },
-      { name: 'color.orange', value: '#f97316' },
-      { name: 'color.pink', value: '#ec4899' },
-      { name: 'color.teal', value: '#14b8a6' },
-      { name: 'color.indigo', value: '#6366f1' },
-      { name: 'color.gray', value: '#6b7280' },
-      { name: 'color.quentin', value: '#ff00ff' },
-    ],
-    skipDuplicates: true,
-  });
-  console.log('Colors created.');
+	await prisma.color.createMany({
+		data: [
+			{ name: "color.red", value: "#ef4444" },
+			{ name: "color.green", value: "#22c55e" },
+			{ name: "color.blue", value: "#3b82f6" },
+			{ name: "color.yellow", value: "#eab308" },
+			{ name: "color.violet", value: "#8b5cf6" },
+			{ name: "color.orange", value: "#f97316" },
+			{ name: "color.pink", value: "#ec4899" },
+			{ name: "color.teal", value: "#14b8a6" },
+			{ name: "color.indigo", value: "#6366f1" },
+			{ name: "color.gray", value: "#6b7280" },
+			{ name: "color.quentin", value: "#ff00ff" },
+		],
+		skipDuplicates: true,
+	});
+	console.log("Colors created.");
 };
 
-await colorSeeding()
-  .then(() => console.log('Color seeding completed successfully.'))
-  .catch((error) => console.error('Error seeding colors:', error));
+colorSeeding()
+	.then(() => console.log("Color seeding completed successfully."))
+	.catch((error) => console.error("Error seeding colors:", error));
