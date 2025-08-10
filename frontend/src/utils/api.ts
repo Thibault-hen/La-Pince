6ab93d05f257d461bd481828be12b48cd931c394
Dxqeee <thibault62118@gmail.com>
2025-08-10 17:21:51 +0200

    fix: removed log when api response status code return 404 or 401 and 500

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -4,3 +4,20 @@ export const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 	withCredentials: true,
 });
+
+api.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		if (
+			error.response?.status === 404 ||
+			error.response?.status === 401 ||
+			error.response?.status === 500
+		) {
+			return Promise.resolve({ data: null, error: true });
+		}
+
+		return Promise.reject(error);
+	},
+);
